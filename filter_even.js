028dd80ffe05538aca2ccbba9d323e3864d23869
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2024-05-18 11:46:09 -0700

    add first problem

---
+++ filter_even.js
@@ -0,0 +1,25 @@
+// write a function that will take an array of numbers as input and remove all odd elements
+// satisfy the following input outputs as listed below
+
+let filter_even = (arr) => {
+    
+}
+
+
+// testcase1
+input1 = [1, 3 ,4]
+output1 = [4]
+
+// testcase2
+input2 = [4, 4 ,4]
+output2 = [4, 4, 4]
+
+// testcase3
+input3 =  [3, 3 , 1]
+output3 =  []
+
+
+console.log(filter_even(input1) === output1);
+console.log(filter_even(input2) === output2);
+console.log(filter_even(input3) === output3);
+
