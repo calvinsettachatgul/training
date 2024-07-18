75abc89309aa00fb257d9235951fa22738d5d1e0
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2024-07-18 16:11:14 -0700

    add python solution

---
+++ filter_even.py
@@ -0,0 +1,26 @@
+## write a function that will take an array of numbers as input and remove all odd elements
+## satisfy the following input outputs as listed below
+## make the false results true
+
+def filter_even(arr):
+    result = []
+    return result
+    
+
+## testcase1
+input1 = [1, 3 ,4]
+output1 = [4]
+
+## testcase2
+input2 = [4, 4 ,4]
+output2 = [4, 4, 4]
+
+## testcase3
+input3 =  [3, 3 , 1]
+output3 =  []
+
+
+print(filter_even(input1) == output1)
+print(filter_even(input2) == output2)
+print(filter_even(input3) == output3)
+
